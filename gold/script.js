a9217e4fcbdf2945afc2345d544219cf531a806d
Lakshmipriya <92292961+Laks52@users.noreply.github.com>
2024-03-17 09:23:32 +0530

    Add files via upload

---
+++ gold/script.js
@@ -0,0 +1,25 @@
+document.getElementById('calculateBtn').addEventListener('click', function() {
+  var gramsInput = parseFloat(document.getElementById('gramsInput').value);
+  var caratValue = parseFloat(document.querySelector('input[name="carat"]:checked').value);
+  var pricePerGram = parseFloat(document.getElementById('price').value);
+  var wastagePercent = parseFloat(document.getElementById('percent').value);
+  var gstPercent = parseFloat(document.getElementById('gst').value);
+
+  if (!isNaN(gramsInput) && !isNaN(caratValue) && !isNaN(pricePerGram) && !isNaN(wastagePercent) && !isNaN(gstPercent)) {
+      var caratPrice = pricePerGram / 24; // Calculate price per carat
+      var totalPriceBeforeWastage = caratValue * caratPrice * gramsInput;
+      var wastageAmount = totalPriceBeforeWastage * (wastagePercent / 100);
+      var totalPriceAfterWastage = totalPriceBeforeWastage + wastageAmount;
+      var GSTamount = totalPriceAfterWastage * (gstPercent / 100);
+      var totalAmount = GSTamount + totalPriceAfterWastage;
+
+      document.getElementById('result').innerHTML =
+          "Total Price Before Wastage: " + totalPriceBeforeWastage.toFixed(2) + " INR<br>" +
+          "Wastage Amount: " + wastageAmount.toFixed(2) + " INR<br>" +
+          "Total Price After Wastage: " + totalPriceAfterWastage.toFixed(2) + " INR<br>" +
+          "GST Amount: " + GSTamount.toFixed(2) + " INR<br>" +
+          "Total Price After Tax and Wastage: " + totalAmount.toFixed(2) + " INR<br>";
+  } else {
+      document.getElementById('result').innerText = "Please enter valid weight, carat, price, wastage percent, and GST percent.";
+  }
+});
